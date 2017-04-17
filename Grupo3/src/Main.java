c0d1e2e9b206246bb2cdd47a5353da0fdbb6e128
nfdar <nfdar@DESKTOP-LH9NU5J>
2017-04-17 10:26:04 +0200

    Clase Main del proyecto Task Manager
    esta en blanco

--- Grupo3/src/Main.java
+++ Grupo3/src/Main.java
@@ -1,32 +1,10 @@
+package com.company;
+
+import com.company.controler.TasksApp;
 
 public class Main {
-	public static void main(String[] args) {
-		
-        System.out.println("Carlos!!!1");
-        
-        System.out.println("Alvaro");
-        
-        System.out.println("Miguel");
-        
-        // Prueba f´natic
-        
-        System.out.println("Alvaroffssfsfewefwsegundamodicif");
-        
-        
-        //ALVARO
 
-
-        
-        
-        //Carlos
-        
-        //otro dia la misma mierda
-
-        
-        
-        
-        
-        //mierda numero2
-    }
+    public static void main(String[] args) {
 
 }
+
