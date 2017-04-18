afc3e8be19fcf577c5629b1bbd441f31b2650380
Alvaro <Alvaro@Alvaro>
2017-04-18 17:55:30 +0200

    Añadimos los atributos de la clase

--- taskManager/src/taskManager/Task.java
+++ taskManager/src/taskManager/Task.java
@@ -2,4 +2,25 @@ package taskManager;
 
 public class Task {
 
+	//ATRIBUTOS
+
+    /**
+     * Task description
+     */
+    private String description;
+    /**
+     * Due date of the task
+     */
+    private Date dueDate;
+    /**
+     * Priority of the task
+     * 1. Low
+     * 2.Medium
+     * 3.Higher
+     */
+    private int priority;
+    /**
+     * True if the task is completed
+     */
+    private boolean complete;
 }
