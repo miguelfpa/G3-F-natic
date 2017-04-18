76290881f5fed4e4d6f7d1e1faaa54bcdc09954c
Alvaro <Alvaro@Alvaro>
2017-04-18 18:00:39 +0200

    Constructores y accesores

--- taskManager/src/taskManager/Task.java
+++ taskManager/src/taskManager/Task.java
@@ -23,4 +23,51 @@ public class Task {
      * True if the task is completed
      */
     private boolean complete;
+    
+  //CONSTRUCTORES
+
+    public Task() {
+
+    }
+
+    public Task(String description, int priority) {
+        this.description = description;
+        this.dueDate = new Date();
+        this.priority = priority;
+        this.complete = false;
+    }
+
+    //ACCESORES
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
 }
